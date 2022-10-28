ebe1dc497c84ce73a9e78ee5f5e30be8b7d4f1a9
fireman <fireman2747@yandex.ru>
2022-10-28 15:13:09 +0800

    add Homework 7 array m*n

---
+++ Homework001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+Console.WriteLine("введите количество строк");
+int lines = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите количество столбцов");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+int[,] numbers = new int[lines, columns];
+FillArrayNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
